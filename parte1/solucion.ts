20e25fe0aa040d0a63a44b3dd5c07dfab33208e4
Nicolas Urruty <nicourruty@gmail.com>
2020-05-28 18:01:25 +0200

    solucion.ts

---
+++ parte1/solucion.ts
@@ -0,0 +1,71 @@
+class RegisteredUser {
+  
+  constructor({id, service = []}){
+    this.id = id
+    this.service = service
+  }
+
+  get id(){
+    return id
+  }
+
+  getTotal() {
+    return this.service.calcTotalUser(this.id)
+  }
+}
+
+interface Service {
+  getTotal: (id: string) => number
+}
+
+interface ContentRepo {
+  getMediaContentByUser(id): (id: string) => MultimediaContent
+}
+
+class StreamingService implements Service {
+  constructor(props){
+    this.mediaRepo = props.mediaRepo
+    this.timestamp = props.timestamp
+  }
+
+  getTotal(id) {
+    const medias = this.mediaRepo.getMediaContentByUser(id)
+    let total = 0
+    medias.map(m => {
+      total = total + m.streamingPrice + m.additionalFee
+    })
+  }
+}
+
+class DownloadService implements Service {
+  constructor(props){
+    this.mediaRepo = props.mediaRepo
+    this.timestamp = props.timestamp
+  }
+
+  getTotal(id) {
+    const medias = this.mediaRepo.getMediaContentByUser(id)
+    let total = 0
+    medias.map(m => {
+      total = total + m.downloadingPrice + m.additionalFee
+    })
+  }
+
+}
+
+class MultimediaContent {
+  constructor(props){
+    this.streamingPrice = props.streamingPrice
+    this.downloadPrice = props.downloadPrice
+    this.additionalFee = 0
+  }
+
+  
+}
+
+class PremiumContent extends MultimediaContent {
+  constructor(props){
+    super(props)
+    this.additionalFee = props.additionalFee
+  }
+}
